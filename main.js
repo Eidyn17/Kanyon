8af25da36c1e664f8651f30304442c803e92d96c
Eidyn17 <145263515+Eidyn17@users.noreply.github.com>
2023-12-20 19:51:56 +0000

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,11 @@ const options = {
 
 const client = mqtt.connect('tls://95ea70d6edb34e4a956c8f346ae8fba8.s1.eu.hivemq.cloud:8884', options);
 
+// Function to publish a message every 5 seconds
+function publishMessage() {
+  client.publish('Kanyon/mqtt_request', '1');
+}
+
 // prints a received message
 client.on('message', function(topic, message) {
   console.log(String.fromCharCode.apply(null, message)); // need to convert the byte array to string
@@ -23,6 +28,12 @@ client.on('error', (error) => {
   console.log('Error:', error);
 });
 
-// subscribe and publish to the same topic
+// subscribe to topics
 client.subscribe('Kanyon/HV_Voltage');
-client.publish('Kanyon/mqtt_request', '1');
+client.subscribe('Kanyon/Temp');
+client.subscribe('Kanyon/LV_soc');
+client.subscribe('Kanyon/LV_Voltage');
+
+// Set up an interval to call the publishMessage function every 5 seconds (5000 milliseconds)
+setInterval(publishMessage, 5000);
+
