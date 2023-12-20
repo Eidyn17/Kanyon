bcb862c6d8fbe46b5d5580ce4028830d2c7b9e38
Eidyn17 <145263515+Eidyn17@users.noreply.github.com>
2023-12-20 18:29:51 +0000

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const options = {
   password: 'Mqtt1111',
   protocol: 'wss', // Specify the WebSocket protocol
   port: 8883,      // Specify the port for WSS
+  path: '/mqtt',   // Specify the WebSocket path
 };
 
 const client = mqtt.connect('tls://95ea70d6edb34e4a956c8f346ae8fba8.s1.eu.hivemq.cloud', options);
