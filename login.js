eb084cb442af7bacb756781296b581f4a9c76e3b
Eidyn17 <145263515+Eidyn17@users.noreply.github.com>
2023-12-22 21:40:00 +0000

    Update login.js

--- login.js
+++ login.js
@@ -1,14 +1,28 @@
-const options = {
-  username: 'Eidyn',
-  password: 'Mqtt1111',
-  protocol: 'wss', // Specify the WebSocket protocol
-  port: 8884,      // Specify the port for WSS
-  path: '/mqtt',   // Specify the WebSocket path
-};
+document.getElementById('login-form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent the form from being submitted normally
 
-const client = mqtt.connect('tls://95ea70d6edb34e4a956c8f346ae8fba8.s1.eu.hivemq.cloud:8884', options);
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
 
-// reassurance that the connection worked
-client.on('connect', () => {
-  console.log('Connected!');
+  const options = {
+    username: username,
+    password: password,
+    protocol: 'wss',
+    port: 8884,
+    path: '/mqtt',
+  };
+
+  const client = mqtt.connect('tls://95ea70d6edb34e4a956c8f346ae8fba8.s1.eu.hivemq.cloud:8884', options);
+
+  client.on('connect', () => {
+    console.log('Connected!');
+    // If connected successfully, redirect to the main page
+    window.location.href = 'main.html';
+  });
+
+  client.on('error', (error) => {
+    console.log('Error:', error);
+    // If there is an error, show an alert
+    alert('Failed to connect. Please check your username and password.');
+  });
 });
